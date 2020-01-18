54a90840cba09d0032504ea021f0fc0455deda72
turu <piotr.turu.turek@gmail.com>
2020-01-18 21:18:56 +0100

    simplify github action and remove env in recreate script only if it exists

--- recreate_environment.sh
+++ recreate_environment.sh
@@ -2,7 +2,11 @@
 
 # Recreates environment based on conda's environment.yml and pip's requirements.txt
 
-conda env remove -n yalla -y
+ENVS=$(conda env list | awk '{print $1}' )
+if [[ $ENVS = *"yalla"* ]]; then
+   source deactivate
+   conda env remove -n yalla -y
+fi
 conda env create -f environment.yml
 source activate yalla
 pip install -r requirements.txt
